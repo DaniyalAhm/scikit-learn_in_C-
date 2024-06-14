e9a04d91446695e8b50f48d5fe4914390ec0c071
Daniyal <daniyala@bu.edu>
2024-06-14 12:36:25 -0500

    gotta fix the formula for binary-bayes

--- Binary-Bayes.cpp
+++ Binary-Bayes.cpp
@@ -24,14 +24,17 @@ class BinaryBayes{
     Eigen:: VectorXd y_train;
     unordered_map<int, vector<vector<string>>> combined_data;
     unordered_map<string, unordered_map<int, double>> P_word_class; // Correct type
+    unordered_map<int, unordered_map<string, int>> Is_word_in_class;
     unordered_map<int, unordered_map<string, int>> feature_counts_by_class;
     unordered_set<string> vocabulary;
     unordered_map<int,int> all_feature_counts_by_class;
 
 
 
+
+
     public:
-        MultinomialBayes(Eigen::VectorXd y_train, vector<string> Raw_data){
+        BinaryBayes(Eigen::VectorXd y_train, vector<string> Raw_data){
             X_train = preprocess_text(Raw_data);
             this->y_train = y_train;
             combine_data();
@@ -131,10 +134,11 @@ class BinaryBayes{
                     all_feature_counts_by_class[class_]= 0;
                 }
 
+
             }
 
 
-            for (auto it = combined_data.begin(); it != combined_data.end(); ++it) {
+   for (auto it = combined_data.begin(); it != combined_data.end(); ++it) {
                     for (int i =0; i< it ->second.size();i++){
                         all_feature_counts_by_class[it->first] += it->second[i].size();
                     
@@ -156,11 +160,55 @@ class BinaryBayes{
             }
 
 
+
+        for(int z=0; z<y_train.size();z++){
+            if(Is_word_in_class.find(y_train(z))==Is_word_in_class.end()){
+                Is_word_in_class[y_train(z)]= unordered_map<string,int>();
+            }
+            for (int i =0; i<X_train.size();i++){
+                for (int j =0; j<X_train[i].size();j++){
+                    string feature= X_train[i][j];
+                    if (Is_word_in_class[class_].find(feature)==Is_word_in_class[class_].end()){
+                        Is_word_in_class[class_][feature]= 0;
+                    }
+
+                }
+
+            }
+        }
+
+            //? This is actually making the dictionary for is word in class
+            for (auto word = combined_data.begin(); word != combined_data.end(); ++word) {
+                int sum = 0;
+                for(int i =0; i<word->second.size();i++){
+                    bool found = false;
+
+
+                    for(int j =0; j<word->second[i].size();j++){
+                        string feature= word->second[i][j];
+
+                        //?word->first is the class
+
+                        //!FIX THIS wrong fornula
+
+                        //?if the word is not found in that document
+                        if(found==false ){
+
+                            Is_word_in_class[word->first][feature]=+1;
+                            found = true;   }
+                                            all_feature_counts_by_class[word->first]+=1;
+
+
+                }
+
+
+
+
             //? Now We calculate the conditional probabilities of each feature
 
 
             for (auto word = vocabulary.begin(); word != vocabulary.end(); ++word) {
-                for (auto it = feature_counts_by_class.begin(); it != feature_counts_by_class.end(); ++it) {
+                for (auto it = all_feature_counts_by_class.begin(); it != all_feature_counts_by_class.end(); ++it) {
                     int class_ = it -> first;
                     if (P_word_class.find(*word)==P_word_class.end()){
                                         P_word_class[*word]= unordered_map<int,double>();
@@ -247,100 +295,5 @@ class BinaryBayes{
         };
 
 };
-
-
-
-
-
-    /*
-    Psuedo-Code:
-        Inputs: Y_train, X_train
-        Function_PreProcess X_train(X_train):
-            Split each word by " " and turn into an array
-            X_train = now a list of words 
-
-
-        Function_ Compute Individual Probablity
-            Probablity_of_class = {}
-            Probablity_of_each_feature = {}
-
-            For every class in Y_train:
-                Probablity_of_class[class]= total_count(class)/len(Y_train)
-
-
-            For each document in X_train:
-                For each feature in document:
-                    Probablity_of_each_feature[word] = total_count[word]/all_words
-
-
-
-        Function_Calculating_Conditional Probability:
-     
-            
-            #Calculating P(feautre1 |spam)
-            Vocab size=(al_words_in_all_docs)
-
-            Probabailitys_Feature_given_Class = {}
-
-
-            !Overall Structure Class_word_counts = {word1:count1, word2:count2...}
-            class_word_counts = dictionary of dictionaries to store count of each word given a class
-
-            !Overall Structure Class_counts = {class:total_word Count1...}
-            class_counts = dictionary to store count of all words for each class
-
-            For each document, class in zip(X_train_processed, Y_train):
-
-
-                For each document, class in zip(X_train_processed, Y_train):
-                        If class not in class_word_counts:
-                            class_word_counts[class] = {}
-                            class_counts[class] = 0   
-
-                        For word in document:
-                            If word not in class_word_counts[class]:
-                                    class_word_counts[class][word] = 0
-                                class_word_counts[class][word] += 1 
-                                class_counts[class] += 1
-
-
-            For word in set_of_words:
-                        Conditional_Probability[word] = {}
-                        For class in class_word_counts:
-                            !Word count by class SUPPORTS MY ORIGINAL LOGIC
-                            word_count = class_word_counts[class][word]
-                            !TOTAL COUNT OF WORDS BY CLASS FOR THE DENOMINATOR
-                            total_count = class_counts[class] #Count of all the world
-
-
-                            !THE MATHIMATICAL FORMULA
-                            P(Feature| Class) = count(feature)+1/total counts of words in that class + overall vocab size
-
-
-
-
-                            !THE BAUETIFUL FORMULA with laplace smoothing
-                            Conditional_Probability[word][class] = (word_count + 1) / (total_count + vocabulary_size)
-                    
-
-        Function Predict
-            #Calculating Proportional Probabilities
-            Probabilties_class_given_feature
-            
-            For each class:
-                 Probabilties_class_given_feature[class]= Probablity_of_class[class]
-                 For each feature in class:
-                        Probabilties_class_given_feature[class]=  Probabilties_class_given_feature* Probabailitys_Feature_given_Class[class][feature] 
-
-
-                # Normalize the probabilities
-                    sum_probabilities = sum(Probabilties_class_given_feature.values()) //!Sum of total probabilties
-                    For class in Probabilties_class_given_feature:
-                        Probabilties_class_given_feature[class] /= sum_probabilities
-                    
-
-                Return Probabilties_class_given_feature
-
-    */
 
 

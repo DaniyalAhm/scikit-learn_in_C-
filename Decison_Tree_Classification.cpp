70c11d4de5a29ea6a4a454bee09b0e14833be66a
Daniyal Ahmed <daniyala@bu.edu>
2024-06-23 01:51:02 -0500

    pushed Gini index psuedo code

--- Decison_Tree_Classification.cpp
+++ Decison_Tree_Classification.cpp
@@ -39,21 +39,19 @@ PsuedoCode:
         
         Find_best_possible_split(node):
             for attribute in Node:
-                thresold = X[:attribute]
-                for 
+                thresold = all_unique_vals([X:attribute]) //? Select all values in that feature column
+                    for threshold in tresholds:
+                        Left_of_threshold //? This is left values of the threshold
+                        Right_of_threshold //?  This is the right values of the threshold
 
+                        if len(left_of_threshold !=0 ) and Right_of_threshold !=0"
+                            gain = Support_function(y, left_indices, right_indices) //? Whether that be gini or info gain
+                                  if gain > best_gain:
+                                    best_gain = gain
+                                    best_split = (feature_index, threshold)
+            return best_split and best_gain
 
-            
-
-
-        
-        
-
-
-
-
-
-
+                        
 
 
 */
---
+++ Gini_index.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+
+/*
+PsuedoCode:
+    ?This is the PsuedoCode FOr the Gini index
+
+    Function Gini_index:
+
+        Gini_index= 0
+        For each class i:
+            Gini_Index += (class_counts(i)/len(dataset))**2
+
+
+        return 1.0 - Gini_index
+
+    Function Gini_score:
+        Gini_score= 0
+        For each class i:
+            Gini_score += (class_counts(i)/len(dataset))**2
+
+        
+    return Gini_score
+
+
+
+*/
+    

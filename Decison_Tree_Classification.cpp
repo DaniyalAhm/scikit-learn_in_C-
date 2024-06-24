a3061b1a46f53a2eddf5291067ecd39e21f59755
Daniyal Ahmed <daniyala@bu.edu>
2024-06-24 10:57:04 -0500

    Almost done with Decision Tree code

--- Decison_Tree_Classification.cpp
+++ Decison_Tree_Classification.cpp
@@ -3,35 +3,89 @@
 #include <vector>
 #include <unordered_map>
 #include <string>
+#include <boost/algorithm/string.hpp>
 
+using namespace std;
 
-struct VectorHash {
-    size_t operator()(const vector<string>& vec) const {
-        size_t hash = 0;
-        for (const auto& str : vec) {
-            hash ^= std::hash<string>()(str) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-        }
-        return hash;
-    }
+struct Node {
+    bool is_leaf;
+    Node* parent;
+    pair<int, int> split; // X_train start index and ending index
+    Node(bool is_leaf, Node* parent = nullptr,    pair<int, int> split) 
+        : is_leaf(is_leaf), parent(parent), pair(pair) {}
 };
 
 
-class Decision_Tree{
+
+class Decision_Tree {
     vector<vector<string>> X_train; // Correct type
-    Eigen:: VectorXd y_train;
+    Eigen::VectorXd y_train;
     unordered_map<int, vector<vector<string>>> combined_data;
     int Max_depth;
 
-
-
 public:
-    Decision_Tree(vector<vector<string>> :: X_train,  Eigen:: VectorXd y_train, int Max_depth=-1, string Splitting_function="Information_Gain", int Min_depth=-1){
+    Decision_Tree(vector<vector<string>> X_train, Eigen::VectorXd y_train, int Max_depth = -1, string Splitting_function = "Information_Gain", int Min_depth = -1) {
         this->X_train = X_train;
         this->y_train = y_train;
         combine_data();
         this->Max_depth = Max_depth;
+    }
+
+private:
+    vector<vector<string>> preprocess_text(vector<string> Raw_data) {
+        vector<vector<string>> result;
+        for (const auto& doc : Raw_data) {
+            vector<string> splitstring;
+            boost::algorithm::split(splitstring, doc, boost::is_any_of(" "));
+            result.push_back(splitstring);
+        }
+        return result;
+    }
+
+    void combine_data() {
+        for (int i = 0; i < y_train.size(); ++i) {
+            int class_ = y_train(i);
+            if (combined_data.find(class_) == combined_data.end()) {
+                combined_data[class_] = vector<vector<string>>(); // Initialize vector if not present
+            }
+            combined_data[class_].push_back(X_train[i]);
+        }
+    }
+
+    bool is_pure(Node node) { // ! Function should return bool
+        // Checks if the label of all the data points in the dataset are the same
+        pair<int, int> thresholds=node.split;
+        int class_ = y_train[thresholds.first];
+        for (int i = thresholds.first; i < thresholds.second;i++ ){
+                if(class_ != y_train[i]){
+                    return false;
+                }
+            return true;
+
+        };
 
 
+
+public:
+    Node Fit_Private(node, int depth) {
+        if (is_pure(node)) { // ! Function `is_pure` expects data, not a node
+            return node; // ! `return node;` does not match the return type `void`
+        }
+
+        if (depth == Max_depth) {
+            return node; // ! `return node;` does not match the return type `void`
+        } else {
+
+            
+
+            vector<vector<string>> best_split = Find_best_possible_split(, );
+            left_node = best_split[0]; // ! This assumes `best_split` is a 2D vector of strings, needs to be adjusted
+            right_node = best_split[1];
+
+
+            node.attach_left(Fit(left_node, depth + 1, node_y)); // ! `attach_left` is not defined, nor is `node`
+            node.attach_right(Fit(right_node, depth + 1, node_y)); // ! `attach_right` is not defined, nor is `node`
+        }
     }
 
 
@@ -39,80 +93,45 @@ public:
 
 
 
-        void combine_data(){
 
-            for (int i =0; i<y_train.size(); ++i){
-                int class_ = y_train(i);
+    vector<Node*, Node*> Find_best_possible_split(node, bool is_leaf) { // ! Missing parameter types
+        pair<int, int> thresholds=node.split;
+        int class_ = y_train[thresholds.first];
+        int best_gain =0;
+        int gain = 0;
+        pair <int, int> new_left_threshold;
+        pair <int, int> new_right_threshold;
 
-                if(combined_data.find(class_) == combined_data.end()){
-                    combined_data[class_] = vector<vector<string>>(); // Initialize vector if not present
+        for (int i = thresholds.first; i < thresholds.second;i++ ){
+            for (int j = 0; j< X_train[i].size(); j++){ //? All the values in that feature column
+                new_left_threshold= <0, j-1>;
+                new_right_threshold = <j, X_train[i].size()-1;>
+
+
+
+                //!Add Support function...
+
 
 
                 }
-                combined_data[class_].push_back(X_train[i]);
+                    
 
-            }
+                
 
-        };
+                                
 
-    private:
-        is_pure(vector<vector<string>> data){
-
-            //Checks if the label of all the data points in the dataset are the same
-            string first_label = data[0][data[0].size()-1];
-            for (int i = 1; i<data.size(); ++i){
-                if(data[i][data[i].size()-1] != first_label){
-                    return false;
-                }
-            }
-            return true;
-        }
-
-    public:
-        void Fit(node, node_data, depth, node_y){
-            if(is_pure(node)){
-                return node;
-            }
-
-
-            if(depth == Max_depth){
-                return node;
-            }
-
-            else{
-                vector<vector<string>> left_node, right_node;
-                vector<vector<string>> best_split = Find_best_possible_split(node_data, node_y);
-                left_node = best_split[0];
-                right_node = best_split[1];
-                node.attach_left(Fit(left_node, depth+1,  node_y));
-                node.attach_right(Fit(right_node, depth+1, node_y));
             }
 
         }
 
 
-        vector<vector<string>> Find_best_possible_split(node_data,node_y){
-
-
-            for(auto attribute )                
-
-
-
-        }
-
-
-
-}
 
 
 
 
 
 
-
-
-
-
+};
 
 
 /*
@@ -173,3 +192,4 @@ PsuedoCode:
 
 
 */
+
